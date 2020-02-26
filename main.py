a6850c48f5e4b8a6cff9ebc1a60d3c8fec84cc51
pokebox <pokebox@qq.com>
2020-02-26 21:42:03 +0800

    添加加热线程，到设定时间自动加热

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import os
 import time
+import RPi.GPIO as GPIO
 # 界面处理
 from demo_opts import get_device
 from luma.core.virtual import terminal
@@ -34,29 +35,36 @@ def is_number(s):
 
 class ysj(object):
     def __init__(self,device, keyboard):
+        GPIO.setmode(GPIO.BCM)
+        self.gpio_beep      = 22    ##蜂鸣器GPIO，自行设置
+        self.gpio_heating   = 18    ##加热器电源IO，自行设置
+
         self.device=device
         self.keyboard=keyboard
         self.mode=0 #模式状态：0正常，1加热设置（温度，水量，时间），2保温设定（温度，水量，时段）
 
         # 加热设定初值
-        self.hot_h=0        ##加热时间
-        self.hot_m=0
-        self.hot_s=0
-        self.hot_temp=0     ##加热温度
-        self.hot_waterVol=0 ##加热水量
+        self.hot_h          = 0 ##加热时间
+        self.hot_m          = 0
+        self.hot_s          = 0
+        self.hot_temp       = 70 ##加热温度
+        self.hot_waterVol   = 0 ##加热水量
 
         # 初始化传感器
-        self.ds18b20=DS18B20()
-        self.hcsr04=HCSR04()
+        self.ds18b20    = DS18B20()
+        self.hcsr04     = HCSR04()
 
-        self.temp=self.ds18b20.read_temp()
-        self.waterLevel=self.hcsr04.distance()
+        self.temp       = self.ds18b20.read_temp()
+        self.waterLevel = self.hcsr04.distance()
         # 初始化线程
-        self.th_temp=Thread(target=self.gettemp)
+        self.th_temp = Thread(target=self.gettemp)
         self.th_temp.setDaemon(True)
 
-        self.th_hc04=Thread(target=self.gethcsr04)
+        self.th_hc04 = Thread(target=self.gethcsr04)
         self.th_hc04.setDaemon(True)
+
+        self.th_heating = Thread(target=self.heatingTask)
+        self.th_heating.setDaemon(True)
 
     def make_font(self, name, size):
         font_path = os.path.abspath(os.path.join(
@@ -73,12 +81,53 @@ class ysj(object):
             self.waterLevel=self.hcsr04.distance()
             time.sleep(0.5)
 
+    def heatingTask(self):
+        while True:
+            if self.hot_h == int(time.strftime("%H", time.localtime())):
+                if self.hot_m == int(time.strftime("%M", time.localtime())):
+                    if self.hot_s == int(time.strftime("%S", time.localtime())):
+                        # 判断温度和水位达到加热要求并且当前没在加热
+                        print(u"开始加热")
+                        print(self.isHeating())
+                        if self.temp <= self.hot_temp and self.isHeating() == False:
+                                self.setBeep(True)
+                                self.setHeating(True)
+                                time.sleep(0.5)
+                                self.setBeep(False)
+            time.sleep(0.2)
+
+    def setHeating(self, open):     ##加热棒
+        GPIO.setup(self.gpio_heating, GPIO.OUT)
+        if open:
+            try:
+                GPIO.output(self.gpio_heating, True)
+            except err:
+                pass
+        else:
+            try:
+                GPIO.output(self.gpio_heating, False)
+            except err:
+                pass
+
+    def isHeating(self):
+        return GPIO.input(self.gpio_heating)
+
+    def setBeep(self, open):        ##蜂鸣器
+        GPIO.setup(self.gpio_beep, GPIO.OUT)
+        if open:
+            GPIO.output(self.gpio_beep, True)
+        else:
+            GPIO.output(self.gpio_beep, False)
+
+
     def main(self):
         font = self.make_font("wqy-zenhei.ttc", 24)
         font_size = font.getsize(u"水")[1]
 
         self.th_temp.start()
         self.th_hc04.start()
+        self.th_heating.start()
+
         # 主循环
         while True:
             # 等待按键操作期间刷新默认屏幕
@@ -182,6 +231,31 @@ class ysj(object):
                                     print(add_mode)
                                 elif self.keyboard.key == "#":
                                     break
+                        elif ret2 == 3:
+                            add_mode=1
+                            while True:
+                                with canvas(self.device) as draw:
+                                    draw.text((0,0), u"加热水量设置", font=font)
+                                    draw.text((0,font_size*1), u"{0} cm".format(self.hot_waterVol), font=font)
+                                    draw.text((0,font_size*2), u"#确定 AD上下 BC左右", font=font)
+                                # 等待按键按下
+                                while self.keyboard.havekey == False:
+                                    time.sleep(0.01)
+                                # 复位按键标识符
+                                self.keyboard.havekey = False
+                                # 判断按下的是数字键
+                                if is_number(self.keyboard.key):
+                                    if add_mode == 1:
+                                        self.hot_waterVol=int(self.keyboard.key)
+                                        add_mode += 1
+                                    elif add_mode == 2:
+                                        self.hot_waterVol=self.hot_waterVol*10+int(self.keyboard.key)
+                                        add_mode += 1
+                                    elif add_mode >= 3:
+                                        break
+                                    print(add_mode)
+                                elif self.keyboard.key == "#":
+                                    break
 
                     elif ret == 2:
                         menu_3=OptionMenu(u"保温设定", self.keyboard, self.device, font=font)
@@ -194,6 +268,12 @@ class ysj(object):
                     elif ret == 0:
                         break
                 self.keyboard.havekey=False
+
+            elif self.keyboard.key == "A":  # 加热开关
+                if self.isHeating():
+                    self.setHeating(False)
+                else:
+                    self.setHeating(True)
 
             time.sleep(0.2)
 
